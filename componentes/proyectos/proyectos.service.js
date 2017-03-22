bc6f33fbd0aab4ad80b71d0646d6a10db3d0e6db
jeremyb20 <jbaccab@ucenfotec.ac.cr>
2017-03-21 19:49:16 -0600

    cambios en el administrador
    se agregaron los cursos que agregó Alex

--- componentes/proyectos/proyectos.service.js
+++ componentes/proyectos/proyectos.service.js
@@ -48,6 +48,8 @@
       }
       localStorage.setItem('proyectosLS',JSON.stringify(listaProyectos));
     }
+
+
     
   }
 
--- componentes/usuario/estudiantes/verProyectos/verProyecto.controller.js
+++ componentes/usuario/estudiantes/verProyectos/verProyecto.controller.js
@@ -2,14 +2,16 @@
   angular
     .module('myEnsamble')
     .controller('verProyectoController',verProyectoController);
-    function verProyectoController(verProyectoService,ImagenService,Upload,$scope){ //se inyecta el service userService en el controlador para que se tenga acceso
+    function verProyectoController(verProyectoService,ImagenService,Upload,$scope, proyectosService){ //se inyecta el service userService en el controlador para que se tenga acceso
       //controlador
       var verProyectoCtrl = this; //binding del controlador con el html, solo en el controlador
         verProyectoCtrl.cloudObj = ImagenService.getConfiguration();
+        verProyectoCtrl.proyectos = proyectosService.setProyectos();
         
       function init(){ // función que se llama así misma para indicar que sea lo primero que se ejecute
         verProyectoCtrl.proyectosList = verProyectoService.getVerProyecto();
       }init();
+
         
      //se establece un objeto de angular normal
 
